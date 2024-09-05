51f9b560355c1144c03fefeeaae603a1b9a02cf4
angel <angel@LAPTOP-VMHEOFKM>
2024-09-05 18:21:06 -0500

    estatura de estudiantes, interfaz

---
+++ src/logica/Estatura_estudiantes_IGU.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+import igu.Pantallaestatura;
+
+
+/**
+ *
+ * @author angel
+ */
+public class Estatura_estudiantes_IGU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+          Pantallaestatura pant1  = new Pantallaestatura();
+        pant1.setVisible(true);
+        pant1.setLocationRelativeTo(null);
+    }
+    
+}
